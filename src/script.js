bcd525d3e6b29f1cf9e1377f6277c1dd24376364
Vatsal Joshi <vatz88@gmail.com>
2020-02-28 18:49:13 +0530

    :sparkles: improved version

--- src/script.js
+++ src/script.js
@@ -2,10 +2,10 @@ var scale = 0.2;
 
 // hard coded for the purpose of example
 var minimapNavigatorPosition = {
-  minX: 66,
-  minY: 587,
-  maxX: 149,
-  maxY: 670,
+  minX: 0,
+  minY: 0,
+  maxX: 80,
+  maxY: 80,
 };
 
 var minimapPaper = new joint.dia.Paper({
@@ -13,7 +13,7 @@ var minimapPaper = new joint.dia.Paper({
   model: graph,
   width: config.paeprWidth * scale,
   height: config.paperHeight * scale,
-  gridSize: 1
+  gridSize: 1,
 });
 
 minimapPaper.scale(scale);
@@ -35,30 +35,39 @@ $('#minimap-navigator').width(containerWidthHeight * scale);
 $('#minimap-navigator').height(containerWidthHeight * scale);
 
 // Bind container scrolling
-$('#container').scroll(function (e) {
-  $('#minimap-navigator').css('top', minimapNavigatorPosition.minY + e.target.scrollTop * scale);
-  $('#minimap-navigator').css('left', minimapNavigatorPosition.minX + e.target.scrollLeft * scale);
+$('#container').scroll(function(e) {
+  $('#minimap-navigator').css(
+    'top',
+    minimapNavigatorPosition.minY + e.target.scrollTop * scale
+  );
+  $('#minimap-navigator').css(
+    'left',
+    minimapNavigatorPosition.minX + e.target.scrollLeft * scale
+  );
 });
 
 // Bind minimap navigator drag
 var dragFlag = false;
 var x = 0;
 var y = 0;
-$('#minimap-navigator').mousedown(function (e) {
+var initialOffset = { x: 0, y: 0 };
+$('#minimap-navigator').mousedown(function(e) {
   dragFlag = true;
-  x = $(this).offset().left - e.clientX;
-  y = $(this).offset().top - e.clientY;
+  x = e.clientX;
+  y = e.clientY;
+  initialOffset.x = e.target.offsetLeft;
+  initialOffset.y = e.target.offsetTop;
 });
-$('#minimap-navigator').mouseup(function () {
+$('#minimap-navigator').mouseup(function() {
   dragFlag = false;
 });
-$('#minimap-container').mouseleave(function () {
+$('#minimap-container').mouseleave(function() {
   dragFlag = false;
 });
-$('#minimap-navigator').mousemove(function (e) {
+$('#minimap-navigator').mousemove(function(e) {
   if (dragFlag) {
-    var newY = e.clientY + y;
-    var newX = e.clientX + x;
+    var newX = initialOffset.x + e.clientX - x;
+    var newY = initialOffset.y + e.clientY - y;
     if (minimapNavigatorPosition.minY > newY) {
       newY = minimapNavigatorPosition.minY;
     }
@@ -71,9 +80,9 @@ $('#minimap-navigator').mousemove(function (e) {
     if (minimapNavigatorPosition.maxX < newX) {
       newX = minimapNavigatorPosition.maxX;
     }
+
     $('#minimap-navigator').css('top', newY);
     $('#minimap-navigator').css('left', newX);
-
     $('#container').scrollLeft((newX - minimapNavigatorPosition.minX) / scale);
     $('#container').scrollTop((newY - minimapNavigatorPosition.minY) / scale);
   }
